be9375db23dffb91a9482fd25eb846252ddb7fb0
Samu Tamminen <samu-tamminen>
2018-06-07 12:55:25 +0300

    refactor

--- judgements/fetch.py
+++ judgements/fetch.py
@@ -1,37 +1,26 @@
 
 from SPARQLWrapper import SPARQLWrapper, JSON
+from sparql_queries import get_judgements, get_judgement_content
 
-query = '''
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX sfcl: <http://data.finlex.fi/schema/sfcl/>
-
-# Query : List judgments
-SELECT ?j WHERE
-{
-?j rdf:type sfcl:Judgment .
-} LIMIT 3
-'''
-
+def extract_id(url):
+    """
+    :param url like http://data.finlex.fi/ecli/kko/1998/138:
+    :return: 1998_138
+    """
+    splitted = url.split('/')
+    return splitted[-2] + '_' + splitted[-1]
 
 sparql = SPARQLWrapper("http://data.finlex.fi/sparql")
+query = get_judgements(2)
 sparql.setQuery(query)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
-fetch_one = '''
-  PREFIX sfcl: <http://data.finlex.fi/schema/sfcl/>
-
-  # Query : Get judgment content
-  SELECT ?content
-  WHERE {
-   <URL/fin/txt> sfcl:text ?content.
-  }
-'''
-
 for result in results["results"]["bindings"]:
     url = result["j"]["value"]
-    sparql.setQuery(fetch_one.replace("URL", url))
+    query = get_judgement_content(url)
+    id = extract_id(url)
+    sparql.setQuery(query)
     content = sparql.query().convert()
-    print(content)
-    #for result in results["results"]["bindings"]:
-    #    print(result)
+    text = content["results"]["bindings"][0]
+    print(text["content"]["value"])
---
+++ judgements/sparql_queries.py
@@ -0,0 +1,37 @@
+
+prefix_sfcl = "http://data.finlex.fi/schema/sfcl/"
+
+get_judgements_query = '''
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX sfcl: <PREFIX_SFCL>
+
+SELECT ?j WHERE
+{
+?j rdf:type sfcl:Judgment .
+} LIMIT N
+'''
+
+fetch_one_judgement = '''
+  PREFIX sfcl: <PREFIX_SFCL>
+
+  SELECT ?content
+  WHERE {
+   <URL/LANGUAGE/txt> sfcl:text ?content.
+  }
+'''
+
+def get_judgements(n):
+    """
+    :param n number of judgements to return:
+    :return: query
+    """
+    return get_judgements_query.replace("PREFIX_SFCL", prefix_sfcl)\
+        .replace("N",str(n))
+
+def get_judgement_content(url, language='fin'):
+    """
+    :param url: identifier of judgement
+    :return: query
+    """
+    return fetch_one_judgement.replace("PREFIX_SFCL", prefix_sfcl) \
+        .replace("URL", url).replace("LANGUAGE", language)
