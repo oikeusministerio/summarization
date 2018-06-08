51733d8c192879fe25639c9e8d95ec5edee08a09
Samu Tamminen <samu-tamminen>
2018-06-08 16:13:07 +0300

    Add simplest interface possible

--- extractive-summary/server.py
+++ extractive-summary/server.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request, send_from_directory, Response
 import json
 from flask.views import MethodView
 from flask_swagger import swagger
@@ -7,7 +7,7 @@ from flask_swagger import swagger
 #inner imports
 from summary.GraphBasedSummary import GraphBasedSummary
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 class SummaryAPI(MethodView):
 
@@ -39,18 +39,28 @@ class SummaryAPI(MethodView):
             description: Summary created
         """
         if 'content' not in request.json or 'summary_length' not in request.json or 'minimum_distance' not in request.json:
-            # body should be validated by swagger
-            return "Please provide content, summary length and minimum_distance"
+            # body should be validated by swagger, but this works also
+            return json.dumps({'success':False, 'error':'Please provide content, summary length and minimum_distance'}), 404, {'ContentType':'application/json'}
 
         text = request.json['content']
         length = int(request.json['summary_length'])
         threshold = float(request.json['minimum_distance'])
         gbs = GraphBasedSummary(text)
 
-        return gbs.summarize(threshold, summary_length=length)
+        summary = gbs.summarize(threshold, summary_length=length)
+        return json.dumps({'success':True, 'summary':summary}), 201, {'ContentType':'application/json'}
 
 summary_view = SummaryAPI.as_view('summaries')
 app.add_url_rule('/summarize', view_func=summary_view, methods=["POST"])
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 @app.route("/spec")
 def spec():
---
+++ extractive-summary/static/js/script.js
@@ -0,0 +1,43 @@
+
+function send(e) {
+    e.preventDefault();
+    document.getElementById("submit_button").disabled = true;
+
+    content = document.getElementById("content").value
+    document.getElementById("output_div").innerHTML = ""
+    summary_length = document.getElementById("summary_length").value
+    document.getElementById("in_progress").innerHTML = "Lähetetty, tässä menee noin 1-2 minuuttia."
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/summarize", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        content: content,
+        summary_length: summary_length,
+        minimum_distance: 0.1
+    }));
+    xhr.onload = function() {
+      document.getElementById("in_progress").innerHTML = ""
+      document.getElementById("submit_button").disabled = false;
+      var data = JSON.parse(this.responseText);
+      if(data.success) {
+        showSummary(data.summary);
+      } else {
+        showError(data.error)
+      }
+    }
+}
+
+// modify this to more pretty
+function showError(text) {
+    document.getElementById("output_div").innerHTML = text
+}
+
+// modify this to more pretty
+function showSummary(text) {
+    document.getElementById("output_div").innerHTML = "TIIVISTELMÄ: \n" + text
+}
+
+function setup() {
+    document.getElementById("submit_button").addEventListener("click", send);
+}
