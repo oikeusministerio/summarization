0a65936219296a52e58e8ac6deaf6f35c71c57e4
Samu Tamminen <samu-tamminen>
2018-06-14 09:39:15 +0300

    similarity between words

--- embeddings/EmbeddingsReader.py
+++ embeddings/EmbeddingsReader.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 def plot_with_labels(low_dim_embs, labels, filename = None):
@@ -38,3 +39,30 @@ class EmbeddingsReader:
         labels = [self.reversed_dictionary[i] for i in range(plot_only)]
 
         plot_with_labels(low_dim_embs, labels, filename)
+
+    def similarity_word_to_word(self, word1, word2, metric="euclidean"):
+        i1 = self.dictionary[word1]
+        i2 = self.dictionary[word2]
+        w1 = self.embeddings[i1].reshape(1,-1)
+        w2 = self.embeddings[i2].reshape(1,-1)
+        return euclidean_distances(w1,w2)
+
+    def similarity_word_to_text(self, word, text, metric="euclidean"):
+        """
+
+        Kobayashi, Hayato, Masaki Noguchi, and Taichi Yatsuka.
+        "Summarization based on embedding distributions."
+        Proceedings of the 2015 Conference on Empirical Methods in Natural Language Processing. 2015.
+
+        3th page, chapter
+        Similarity Based on Embedding Distributions
+
+        :param word: word
+        :param text: text as list of words
+        :param metric:
+        :return:
+        """
+        i = self.dictionary[word]
+        w = self.embeddings[i].reshape(1, -1)
+        text_embeddings = [self.embeddings[self.dictionary[wi]] for wi in text if wi in self.dictionary]
+        return euclidean_distances(w, text_embeddings).min()
