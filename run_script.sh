f99e5202f3fb869d7eab466ace54eaa89c2ab4e4
Samu Tamminen <samu-tamminen>
2018-06-19 14:00:13 +0300

    connect redis from docker

--- run_script.sh
+++ run_script.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "prod" ]
 then
     docker build -t extractive .
-    docker run -d -p 5000:5000 extractive
+    docker run --net="host" -d -p 5000:5000 extractive
 else
     python3 -m pip install --user -r requirements.txt
     python3 server.py
